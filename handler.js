334b9b643b19e98ddf7d74bea5171715eb86bdf7
Who Is Pino? <87032037+PinoRecode@users.noreply.github.com>
2022-06-22 04:08:34 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -737,7 +737,7 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang 1'))
-                                this.sendButtonImg(id, pp, text, "©ArullOfc", "Okee", "wkwk", null)
+                                this.sendButtonImg(id, pp, text, "©PinoMD", "Okee", "wkwk", null)
                                 }
                     }
                 }
@@ -775,8 +775,8 @@ Untuk mematikan fitur ini, ketik
 
 global.dfail = (type, m, conn) => {
     let msg = {
-        rowner: '*Perintah ini Khusus ArullOfc*',
-        owner: '*Perintah ini Khusus ArullOfc*',
+        rowner: '*Perintah ini Khusus Owner*',
+        owner: '*Perintah ini Khusus Owner*',
         mods: '*Perintah ini hanya dapat digunakan oleh* _*Moderator*_ !',
         premium: '*Perintah ini hanya untuk member* _*Premium*_ !',
         group: '*Perintah ini hanya dapat digunakan di grup!*',
